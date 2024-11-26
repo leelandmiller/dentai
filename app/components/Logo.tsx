7d3adf5f411f6e918ec405b3925ef6c7223ca742
Leeland Miller <leelandmiller@gmail.com>
2024-11-25 18:06:00 -0600

    initial commit, installed supabase, remix

---
+++ app/components/Logo.tsx
@@ -0,0 +1,22 @@
+// app/components/Logo.tsx
+export default function Logo() {
+  return (
+    <svg
+      width="120"
+      height="40"
+      viewBox="0 0 120 40"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <text
+        x="10"
+        y="30"
+        fontFamily="Arial, sans-serif"
+        fontSize="28"
+        fontWeight="bold"
+        fill="currentColor"
+      >
+        Dentai
+      </text>
+    </svg>
+  )
+}
